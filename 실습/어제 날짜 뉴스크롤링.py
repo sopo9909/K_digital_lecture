f01c1839a9247a69de8654ab94233b9805210d78
sopo9909 <63348466+sopo9909@users.noreply.github.com>
2021-02-16 11:21:41 +0900

    Add files via upload

---
+++ 실습/어제 날짜 뉴스크롤링.py
@@ -0,0 +1,17 @@
+import re
+
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime,timedelta
+print(datetime.today().day)
+yesterday = datetime.today() - timedelta(1)
+urls ='https://news.naver.com/main/list.nhn?mode=LPOD&mid=sec&oid=003&date='+str(yesterday.strftime("%Y%m%d"))
+print(urls)
+response = requests.get(urls,headers={'User-Agent':'Mozilla/5.0'})
+soup = BeautifulSoup(response.text, "html.parser")
+result = soup.select(".type06_headline a")
+for v in range(1,len(result),2):
+    print(result[v].text.strip())
+result2 = soup.select(".lede")
+for v in range(len(result2)):
+    print(result2[v].text)
---
+++ 실습/호준님코드변경2.py
@@ -0,0 +1,19 @@
+new_list = []
+while True:
+    a = int(input("메뉴 선택 1-입력,2-조회,3-삭제,0-종료 : "))
+    if a==1 :
+        name = input("이름 : ")
+        math = input("수학 : ")
+        science = input("과학 : ")
+        english = input("영어 : ")
+        new = ("이름 : {}, 수학 : {}, 과학 : {}, 영어 : {}".format(name,math,science,english))
+        new_list.append(new)
+    elif a==2 :
+        print(new_list)
+    elif a ==3:
+        print(new_list)
+        b = int(input("삭제할 번호를 입력해주세요"))
+        new_list.pop(b)
+        print('삭제가 완료되었습니다.')
+    else :
+        print("종료")
