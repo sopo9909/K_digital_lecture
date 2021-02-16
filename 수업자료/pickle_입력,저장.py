e89fcbb53f0c587d89364a413061473512acccc3
sopo9909 <63348466+sopo9909@users.noreply.github.com>
2021-02-16 11:14:49 +0900

    Add files via upload

---
+++ 수업자료/pickle_입력,저장.py
@@ -0,0 +1,19 @@
+import pickle
+name = "tom"
+age = 24
+address = "서울시 마포구"
+scores={"python":90,"deeplearning":95,"database":85}
+
+with open("data2.pkl","wb")as file:
+    pickle.dump(name,file)
+    pickle.dump(age,file)
+    pickle.dump(address,file)
+    pickle.dump(scores,file)
+
+import pickle
+with open("data2.pkl","rb")as file:
+    name2=pickle.load(file)
+    age2=pickle.load(file)
+    address2=pickle.load(file)
+    scores2=pickle.load(file)
+    print(name2)
---
+++ 수업자료/공공데이터.py
@@ -0,0 +1,12 @@
+import requests
+url = 'http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd'
+service_key="t0EiubcVT8TvICxW4e0wQVifJYkvuEW8paf7BSQYPZ%2FMAqJo%2BFXwWwDq6xUzg7CQ5h2JlIb06ASNlQSOVQdsqw%3D%3D"
+service_key_decoded =requests.utils.unquote(service_key)
+req_parameter ={"ServiceKey":service_key_decoded,"searchSe":"road","srchwrd":"반포대로 201"}
+result =requests.get(url,params=req_parameter)
+print(result.text)
+
+import xmltodict
+dict_data = xmltodict.parse(result.text)
+print(dict_data)
+print(dict_data['NewAddressListResponse']['newAddressListAreaCd']['zipNo'])
